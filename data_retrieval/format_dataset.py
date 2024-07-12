80c467a566b33e4d077c53636d178ff262b1b9a5
Yannick Erpelding <yannick.erpelding@gmail.com>
2024-07-13 00:22:08 +0200

    Merged Model, Website and Data Retrieval into one branch

---
+++ data_retrieval/format_dataset.py
@@ -0,0 +1,22 @@
+#Change a few things in the df:
+import pandas as pd
+
+# Read the original DataFrame
+df = pd.read_csv('random_songs.csv')
+
+# Original mappings
+key_conversion = {"C": 0, "C#": 1, "D": 2, "D#": 3, "E": 4, "F": 5, "F#": 6, "G": 7, "G#": 8, "A": 9, "A#": 10, "B": 11}
+mode_conversion = {"Major": 1, "Minor": 0}
+time_signature_conversion = {"3/4": 3, "4/4": 4, "5/4": 5, "6/4": 6, "7/4": 7}
+
+# Reverse mappings
+reverse_key_conversion = {v: k for k, v in key_conversion.items()}
+reverse_mode_conversion = {v: k for k, v in mode_conversion.items()}
+reverse_time_signature_conversion = {v: k for k, v in time_signature_conversion.items()}
+
+# Apply reverse mappings
+df['key'] = df['key'].map(reverse_key_conversion)
+df['mode'] = df['mode'].map(reverse_mode_conversion)
+df['time_signature'] = df['time_signature'].map(reverse_time_signature_conversion)
+
+df.to_csv('songs.csv', index=False)
